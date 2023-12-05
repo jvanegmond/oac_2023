0712b0220595285bb7acebbff76842f5a4c24827
Jos van Egmond <info@josvanegmond.nl>
2023-12-05 21:05:07 +0100

    day 4: use built-in cache

--- day4/solve2.py
+++ day4/solve2.py
@@ -1,4 +1,5 @@
 import re
+from functools import cache
 
 with open("day4/input") as file:
     puzzle = file.readlines()
@@ -10,16 +11,11 @@ for line in puzzle:
     haves = parsed.group(2).split()
     matches.append(sum(1 for n in haves if n in winners))
 
-copies_taken_cache = [None] * len(matches)
-
-def get_copies_taken(card):
-    if copies_taken_cache[card] != None: return copies_taken_cache[card]
-    
+@cache
+def get_copies_taken(card):    
     copies_taken = 1
     for n in range(card + 1, min(card + matches[card] + 1, len(matches))):
         copies_taken += get_copies_taken(n)
-    
-    copies_taken_cache[card] = copies_taken
     return copies_taken
 
 answer = sum(get_copies_taken(card) for card in range(0, len(matches)))
